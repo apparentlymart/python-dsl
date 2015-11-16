a9df84f1018ab100ee9938cd381367d34256b134
Martin Atkins <mart@degeneration.co.uk>
2015-11-15 22:02:00 -0800

    Boilerplate

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+
+setup(
+    name="dsl",
+    version="0.0.0",
+    description="A Python DSL for creating data/configuration DSLs",
+    author="Martin Atkins",
+    author_email="mart@degeneration.co.uk",
+    url="https://github.com/apparentlymart/python-dsl",
+    packages=["dsl"],
+)
